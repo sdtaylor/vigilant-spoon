d6998ccfe559ea40d3d7dd68046557fadab9c16f
Shawn <shawntaylor@weecology.org>
2016-12-08 13:26:06 -0500

    give raster layers nicer names

--- process_raster_layers.R
+++ process_raster_layers.R
@@ -14,17 +14,19 @@ stacked = stack(file_list)
 stacked = crop(stacked, study_area)
 stacked = stack(stacked)
 
-#Convert teh layer name to just the first word of the original filename
+layer_names=c()
+#Convert the layer name to just the first word of the original filename
 #ie. from cv_01_05_1km_uint16 to cv
 for(layer_num in 1:length(stacked@layers)){
-  layer_name = stacked[[layer_num]]@data@names
-  layer_name = stringr::word(layer_name, 1, 1, sep='_')
-  stacked[[layer_num]]@data@names = layer_name
+  layer_names = c(layer_names, stacked[[layer_num]]@data@names)
+  layer_names[layer_num] = stringr::word(layer_names[layer_num], 1, 1, sep='_')
+  stacked[[layer_num]]@data@names = layer_names[layer_num]
   
 }
 
-writeRaster(stacked, './data/habitat_layers', format = 'GTiff')
+writeRaster(stacked, './data/habitat', format = 'GTiff', bylayer=T, suffix=layer_names)
 
+##################################################
 #Make the urban landcover a seperate file
 urban_full = raster('~/data/consensus_landcover/consensus_full_class_9.tif')
 
@@ -38,19 +40,28 @@ file_list = list.files('~/data/consensus_landcover/', full.names = T)
 file_list=file_list[!grepl('xml', file_list)]
 study_area=readOGR('./gis', 'study_area')
 
+actual_layer_names = c('class_1'='Conifer', 'class_2'='Evergreen_Broadleaf', 'class_3'='Decid_Broadleaf', 'class_4'='Mixed_Trees',
+                       'class_5'='Shrubs', 'class_6'='Herbaceous', 'class_7'='Cultivated', 'class_8'='Wetlands', 'class_9'='Urban',
+                       'class_10'='Snow', 'class_11'='Barren', 'class_12'='Water')
 
 stacked = stack(file_list)
 
 stacked = crop(stacked, study_area)
 stacked = stack(stacked)
 
+layer_names=c()
 #Convert teh layer name to just the first word of the original filename
 #ie. from cv_01_05_1km_uint16 to cv
 for(layer_num in 1:length(stacked@layers)){
-  layer_name = stacked[[layer_num]]@data@names
-  layer_name = stringr::word(layer_name, 3, 4, sep='_')
-  stacked[[layer_num]]@data@names = layer_name
+  layer_names = c(layer_names, stacked[[layer_num]]@data@names)
+  layer_names[layer_num] = stringr::word(layer_names[layer_num], 3, 4, sep='_')
+  stacked[[layer_num]]@data@names = layer_names[layer_num]
   
 }
 
-writeRaster(stacked, './data/landcover', format = 'GTiff')
+#Replace class_x with above actual names. Probably a better way to do this. 
+for(i in 1:length(layer_names)){
+  layer_names[i] = as.character(actual_layer_names[layer_names[i]])
+}
+
+writeRaster(stacked, './data/landcover', format = 'GTiff', bylayer=T, suffix=layer_names)
